deeeaf47f522791b06500069bbb7f41ee4411cbd
mohdshoaibkhan <mohdshoaibkhan7211@gmail.com>
2024-08-24 01:19:25 +0530

    app.'/'

--- Server/server.js
+++ Server/server.js
@@ -74,6 +74,10 @@ app.use((err, req, res, next) => {
   res.status(500).send({ success: false, message: "Internal Server Error" });
 });
 
+app.get("/", (req, res) => {
+  res.send("<h1>my name is shoaib khan</h1>");
+});
+
 // Start the server on port 8000
 app.listen(8000, () => {
   console.log("Server is running on port 8000");
