2dc692cf08fcb3de20f176c56b13500175bf74e5
mohdshoaibkhan <mohdshoaibkhan7211@gmail.com>
2024-08-24 01:04:11 +0530

    vercel for deploy

--- Server/server.js
+++ Server/server.js
@@ -32,6 +32,15 @@ app.use(express.json());
 // Enable CORS
 app.use(cors());
 
+app.use(
+  cors({
+    origin: ["https://deploy.mern-1whq.vercel.app"],
+
+    methods: ["POST", "GET"],
+    credentials: true,
+  })
+);
+
 // Connect to the database
 connectDB();
 
